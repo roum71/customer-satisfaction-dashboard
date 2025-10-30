bd295889ba1552f18fa6100ac7798e8998cf0eb7
roum71 <88746941+roum71@users.noreply.github.com>
2025-10-30 07:26:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def detect_nps(df):
 # =========================================================
 # FILTERS
 # =========================================================
-filter_cols = [c for c in df.columns if any(k in c.upper() for k in ["GENDER", "SERVICE", "SECTOR", "NATIONALITY","ACACDEMIC_LEVEL"])]
+filter_cols = [c for c in df.columns if any(k in c.upper() for k in ["GENDER", "SERVICE", "SECTOR", "NATIONALITY","ACADEMIC"])]
 filters = {}
 
 df_filtered = df.copy()
@@ -685,6 +685,7 @@ with tab_pareto:
                            data=pareto_buffer.getvalue(),
                            file_name=f"Pareto_{datetime.now().strftime('%Y%m%d_%H%M')}.xlsx",
                            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
 
 
 
